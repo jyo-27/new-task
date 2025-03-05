51bd3937fd38b5a5cf83dac118d5c467a375eb5e
jyo-27 <jyo2702123@gmail.com>
2025-03-05 23:34:04 +0530

    Update login_page_test_info.py

--- automation/tests/login_page_dertails_test/login_page_test_info.py
+++ automation/tests/login_page_dertails_test/login_page_test_info.py
@@ -3,13 +3,19 @@ from automation.practise_pages.login_page.login_page import LoginPage
 from playwright.sync_api import Page
 from playwright.sync_api import expect
 from automation.practise_pages.login_page.login_locators import LoginLocators
-
+import json
 
 def test_login_page_concept(page: Page):
     page.goto(Data.login_page_url)
     login_page = LoginPage(page)
-    login_page.login_username(Data.login_user_name)
-    login_page.login_password(Data.login_password)
+    with open('log.creds') as f:
+        creds = json.load(f)
+    
+    # Use the credentials for login
+    username = creds["username1"]
+    password = creds["username_password1"]
+    login_page.login_username(username)
+    login_page.login_password(password)
     page.screenshot(path="screenshots/loginpage.png")
     login_page.login_button()
     expect(page.locator(LoginLocators.sucess_login_button)).to_have_text(
